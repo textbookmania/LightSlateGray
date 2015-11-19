e37e9d31926a2199ffdd01bf9eed225f69d1d96b
Rory Andrews <rory00@gmail.com>
2015-11-18 23:23:34 -1000

    #3 Added a profile template
    Displays users profile image, first name, last name, and email (for
    now).

---
+++ app/client/subscriptions/profilesub.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Rory on 11/18/2015.
+ */
+Meteor.subscribe()
--- app/server/profiles/profiles.js
+++ app/server/profiles/profiles.js
@@ -1,6 +1,9 @@
 /**
  * Created by Rory on 11/18/2015.
  */
+Meteor.publish('userData', function () {
+  return Meteor.users.find({}, {fields: {profile: 1}});
+});
 
 Meteor.methods({
   changeEmail: function(email) {
