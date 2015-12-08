83b8acc3e1c7e89452c5d3d5737bdfd4b3986a87
Rory Andrews <rory00@gmail.com>
2015-12-08 01:20:56 -1000

    #42 Changed profile edit to use autoform.

--- app/client/templates/pages/ProfileEdit.js
+++ app/client/templates/pages/ProfileEdit.js
@@ -1,15 +1,24 @@
 /**
  * Created by Rory on 11/18/2015.
  */
-Template.ProfileEdit.events({
-  'submit form': function(e) {
-    e.preventDefault();
+AutoForm.hooks({
+  EditProfileForm: {
+    /**
+     * After successful form submission, go to the ListStuff page.
+     * @param formType The form.
+     * @param result The result of form submission.
+     */
+    onSuccess: function(formType, result) {
+      Router.go('ProfilePage', data=Meteor.user());
+    }
+  }
+});
 
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.profileImage": $(e.target).find('[id=profile-image]').val()}});
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.firstName": $(e.target).find('[id=first-name]').val()}});
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.lastName": $(e.target).find('[id=last-name]').val()}});
-
-    Meteor.call('changeEmail', $(e.target).find('[id=input-email]').val());
-    Router.go('ProfilePage', data=Meteor.user());
+Template.ProfileEdit.helpers({
+  users: function () {
+    return Meteor.users;
+  },
+  userSchema: function () {
+    return Schema.User;
   }
 });
