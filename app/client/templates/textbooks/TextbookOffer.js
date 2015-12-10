6c1842d6d343465816853be2435822cf488c6bbd
Reed Shinsato <rshin808@yahoo.com>
2015-12-10 02:59:59 -1000

    Fix Template

--- app/client/templates/textbooks/TextbookOffer.js
+++ app/client/templates/textbooks/TextbookOffer.js
@@ -1,3 +1,12 @@
 /**
  * Created by Reed on 12/10/2015.
  */
+Template.ListTextbook.helpers({
+
+  /**
+   * @returns {*} All of the Textbook documents.
+   */
+  textbookList: function () {
+    return Textbook.find();
+  },
+});
