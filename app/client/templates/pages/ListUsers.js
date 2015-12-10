c30e823010ab361fa82f254f2c262f8d32f7d952
Rory Andrews <rory00@gmail.com>
2015-12-09 22:19:21 -1000

    #44 Added search bar to user list page.

--- app/client/templates/pages/ListUsers.js
+++ app/client/templates/pages/ListUsers.js
@@ -3,6 +3,22 @@
  */
 Template.ListUsers.helpers({
   getUsers: function () {
-    return Meteor.users.find();
+    if (Router.current().params.query.user) {
+      var name = Router.current().params.query.user;
+      return Meteor.users.find({username:name});
+    }
+    else {
+      return Meteor.users.find();
+    }
+  }
+});
+
+Template.ListUsers.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var name = $(e.target).find('[id=searchname]').val();
+    var q = "user=".concat(name);
+    Router.go("ListUsers", {}, {query: q});
   }
 });
