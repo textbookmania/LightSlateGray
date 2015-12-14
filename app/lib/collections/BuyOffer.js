9c2cdba2f48d4114f0082523a217abf49a8dcbeb
Rory Andrews <rory00@gmail.com>
2015-12-13 15:14:07 -1000

    Fixed student field when creating offers.

--- app/lib/collections/BuyOffer.js
+++ app/lib/collections/BuyOffer.js
@@ -13,7 +13,7 @@ Meteor.methods({
    * @param doc The BuyOffer document.
    */
   addBuyOffer: function (doc) {
-    //doc.student = Meteor.user().profile.username;
+    doc.student = Meteor.user().username;
     if (_.findWhere(BuyOffer.find().fetch(), {student: doc.student, book: doc.book}) || _.findWhere(SellOffer.find().fetch(), {student: doc.student, book: doc.book}) ) {
       if (Meteor.isClient) {
         alert("You already have a sell offer or buy offer for that book.");
