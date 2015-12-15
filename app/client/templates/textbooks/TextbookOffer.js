ffd4375cf4c4dab921307144c45c553962c18a87
Reed Shinsato <rshin808@yahoo.com>
2015-12-15 00:40:55 -1000

    Adding Delete

--- app/client/templates/textbooks/TextbookOffer.js
+++ app/client/templates/textbooks/TextbookOffer.js
@@ -15,3 +15,17 @@ Template.TextbookOffer.helpers({
     return BuyOffer.find({book: this.title.toString()});
   }
 });
+
+Template.TextbookOffer.events({
+  'click .delete': function (e) {
+    if (_.contains(Meteor.user().roles, "admin") === false) {
+      throw new Meteor.Error(403, "Permission Denied");
+    }
+    e.preventDefault();
+    if (confirm("Delete Textbook?")) {
+      var currentTextbookId = this._id;
+      Meteor.call("deleteTextbook", currentTextbookId);
+      Router.go('ListTextbook');
+    }
+  }
+});
